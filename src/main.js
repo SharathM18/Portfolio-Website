b69b3b86c378998ba9c2186c2b5a92ee511a5542
Sharath M <sharathm5684@gmail.com>
2024-02-24 20:57:30 +0530

    implemented contact section

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import "../styles/style.css";
 import "../styles/utilities.css";
 import "../styles/components/projects.css";
 import "../styles/components/skills.css";
+import "../styles/components/contact.css";
