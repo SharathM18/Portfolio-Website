22b5013fd464943207ba471fb2e7947971bcdbac
Sharath M <sharathm5684@gmail.com>
2024-02-24 21:37:01 +0530

    implemented certificates & achievements section

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "../styles/style.css";
 import "../styles/utilities.css";
+import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/projects.css";
 import "../styles/components/skills.css";
+import "../styles/components/certificates.css";
 import "../styles/components/contact.css";
