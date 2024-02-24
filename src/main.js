4a6e0d041434867618ec0c55141b442a5f7d7950
Sharath M <sharathm5684@gmail.com>
2024-02-24 21:49:54 +0530

    implemented navigation bar feature

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import "../styles/style.css";
 import "../styles/utilities.css";
+import "../styles/components/nav.css";
+import "../styles/components/mobile-nav.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/projects.css";
 import "../styles/components/skills.css";
 import "../styles/components/certificates.css";
 import "../styles/components/contact.css";
+
+import responsiveNsvBar from "./utilities/mobile-nav";
+
+responsiveNsvBar();
---
+++ src/utilities/mobile-nav.js
@@ -0,0 +1,26 @@
+const headerBtn = document.querySelector(".header-bar");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileLinks = document.querySelectorAll(".mobile-nav-link");
+
+let isMobileNavOpen = false;
+
+export default function responsiveNsvBar() {
+  headerBtn.addEventListener("click", () => {
+    isMobileNavOpen = !isMobileNavOpen;
+    if (isMobileNavOpen) {
+      mobileNav.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+    } else {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+
+    mobileLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        isMobileNavOpen = false;
+        mobileNav.style.display = "none";
+        document.body.style.overflowY = "auto";
+      });
+    });
+  });
+}
