4129b3a9e284e31f9f40657f92088d9f6067e570
Sharath M <sharathm5684@gmail.com>
2024-02-24 21:12:56 +0530

    implemented about  section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "../styles/style.css";
 import "../styles/utilities.css";
+import "../styles/components/about.css";
 import "../styles/components/projects.css";
 import "../styles/components/skills.css";
 import "../styles/components/contact.css";
