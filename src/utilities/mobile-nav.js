724e70d1c79f22d9c6262b17b7deecf7c7fb7bf6
Sharath M <sharathm5684@gmail.com>
2024-09-29 15:35:29 +0530

    Added Projects, Updated resume and about section and also fixed UI issues

--- src/utilities/mobile-nav.js
+++ src/utilities/mobile-nav.js
@@ -9,17 +9,17 @@ export default function responsiveNsvBar() {
     isMobileNavOpen = !isMobileNavOpen;
     if (isMobileNavOpen) {
       mobileNav.style.display = "flex";
-      document.body.style.overflowY = "hidden";
+      // document.body.style.overflowY = "hidden";
     } else {
       mobileNav.style.display = "none";
-      document.body.style.overflowY = "auto";
+      // document.body.style.overflowY = "auto";
     }
 
     mobileLinks.forEach((link) => {
       link.addEventListener("click", () => {
         isMobileNavOpen = false;
         mobileNav.style.display = "none";
-        document.body.style.overflowY = "auto";
+        // document.body.style.overflowY = "auto";
       });
     });
   });
